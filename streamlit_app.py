a3037e516fa1a6e8c9ceaf05eb8ea14e611542b7
Jennifer-s-fitness <145551959+Jennifer-s-fitness@users.noreply.github.com>
2023-09-27 08:25:25 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
-
 streamlit.text(fruityvice_response)
 
 #create the repeatable code block (called a function)
