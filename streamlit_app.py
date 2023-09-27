433cb1497eaaab01bb4e3775d550df2fa086d4fe
Jennifer-s-fitness <145551959+Jennifer-s-fitness@users.noreply.github.com>
2023-09-27 09:05:46 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,7 +58,7 @@ def get_fruit_load_list():
 #add a button to load the fruit
 if streamlit.btton('Get Fruit Load List'):
    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-    my_data_rows = get_fruit_load_list()
+  my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
